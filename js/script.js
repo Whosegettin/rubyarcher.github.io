e99adbf24309d17a6f1c8f2680673bd32ba55442
Tom Dickinson <87288217+Whosegettin@users.noreply.github.com>
2025-07-17 15:52:05 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+
+const hamMenu = document.querySelector('.ham-menu');
+
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+  hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
+})
+
+
 document.addEventListener('DOMContentLoaded', () => {
   //
   // ── 1. ☰ MENU PANEL TOGGLE ────────────────────────────────────────────────
